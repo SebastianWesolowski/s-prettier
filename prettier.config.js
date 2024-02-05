761a1e4450ab550cb34aa71627232c9098a685de
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2024-02-05 23:29:25 +0100

    chore: 🔧 repair commit structure

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: "always",
+  endOfLine: "auto",
+  jsxSingleQuote: true,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  useTabs: false,
+};
